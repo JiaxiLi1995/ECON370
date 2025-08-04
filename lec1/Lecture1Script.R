64d05f7a02c648371538f1709eac3f71617af235
Jiaxi Li <lijiaxi1995@gmail.com>
2025-08-04 12:00:45 -0400

    Lec1 email disclaimer

--- lec1/Lecture1Script.R
+++ lec1/Lecture1Script.R
@@ -1,3 +1,4 @@
+################################################################################
 print("Hello World!")
 
 # print("Hello World!")
@@ -5,6 +6,11 @@ print("Hello World!")
 # # is for commenting. It is a good habit to comment your code so
 # your future self can better understand and reuse your code
 
+# A line of # can even divide sections of the code and act as a break line!
+# Left of the break line, you can find a little triangle.
+# Click and see what does it do.
+
+# Here is our first dataset
 # Collect the student information
 Student_Info = data.frame(
   Name = c(),
@@ -18,11 +24,14 @@ Student_Info = data.frame(
   Program_exper = c()
 )
 
-
+################################################################################
+# introduction to ggplot2
+# Download packages and load them
 install.packages(c("ggplot2","gapminder"))
 library(ggplot2)
 library(gapminder)
 
+# head and tail help you to get a feel about the data
 head(gapminder)
 gapminder
 
@@ -44,28 +53,33 @@ ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
   geom_point(size = 3, col="black", alpha = 0.3)
 
 
+# Now, we will build a ggplot layer by layer
+# Here is our foundation layer
 p = ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp))
 p
 
-
+# Add dotplot and smoothed line
 p + geom_point(alpha = 0.3)  +
   geom_smooth(method = "loess") 
 p + geom_point(aes(size = pop, col = continent), alpha = 0.3)  +
   geom_smooth(method = "loess") 
 
 
+# Here is a bad plot. Note the foundation layer applies to all future layers
+# You need to make sure it only has the universal stuff applicable to all
 p_bad = ggplot(data = gapminder, 
                aes(x = gdpPercap, y = lifeExp, size = pop, col = continent)) +
   geom_point(alpha = 0.3)  +
   geom_smooth(method = "loess") 
 p_bad
 
-
+# Here is a density plot, what does it do?
 p + geom_density()
 
 ggplot(data = gapminder) + ## i.e. No "global" aesthetic mappings"
   geom_density(aes(x = gdpPercap, fill = continent), alpha=0.3)
 
+# A fancier plot using the same foundation
 p2 =
   p +
   geom_point(aes(size = pop, col = continent), alpha = 0.3) +
